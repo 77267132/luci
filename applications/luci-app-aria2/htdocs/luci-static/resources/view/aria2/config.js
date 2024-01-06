cd6696f073b1222ab96106ec7c10e214f5229d99
77267132 <155213818+77267132@users.noreply.github.com>
2024-01-06 18:58:24 +0800

    Update config.js
    Signed-off-by: 77267132 <155213818+77267132@users.noreply.github.com>

--- applications/luci-app-aria2/htdocs/luci-static/resources/view/aria2/config.js
+++ applications/luci-app-aria2/htdocs/luci-static/resources/view/aria2/config.js
@@ -185,8 +185,8 @@ return view.extend({
 	render: function(aria2) {
 		var m, s, o;
 
-		m = new form.Map('aria2', '%s - %s'.format(_('Aria2'), _('Settings')), '<p>%s</p><p>%s</p>'.format(
-			_('Aria2 is a lightweight multi-protocol &amp; multi-source, cross platform download utility.'),));
+		m = new form.Map('aria2', '%s - %s'.format(_('Aria2'), _('Settings')), '<p>%s</p>'.format(
+			_('Aria2 is a lightweight multi-protocol &amp; multi-source, cross platform download utility.')));
 
 		s = m.section(form.TypedSection);
 		s.title = '%s - %s'.format(_('Aria2'), _('Running Status'));
