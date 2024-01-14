958f014d4334468390e862e315566ab53d93ba21
77267132 <155213818+77267132@users.noreply.github.com>
2024-01-14 16:54:43 +0800

    Update config.js
    Signed-off-by: 77267132 <155213818+77267132@users.noreply.github.com>

--- applications/luci-app-aria2/htdocs/luci-static/resources/view/aria2/config.js
+++ applications/luci-app-aria2/htdocs/luci-static/resources/view/aria2/config.js
@@ -30,7 +30,7 @@ CBIAria2Status = form.DummyValue.extend({
 		poll.add(function() {
 			return Promise.all([
 				callServiceList('aria2', extra)
-				。then(function(res) {
+				.then(function(res) {
 					return E('p', {}, E('em', {}, res.running
 						? _('<font color="green" size="1.8rem">') + ('Aria2 服务正在运行。') + ('</font>')
 						: _('<font color="red" size="1.8rem">') + ('Aria2 服务未运行。') + ('</font>'))
